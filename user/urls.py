e7236715a110737f1aaa95a66a0c98f50d25beab
tauyoung <tauyoung@foxmail.com>
2022-11-27 22:54:20 +0800

    Add register page and redirect

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'user'
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('login', views.login, name='login')
+	path('login', views.login, name='login'),
+	path('register', views.register, name='register')
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .forms import *
 
 # Create your views here.
 def index(request):
-	return HttpResponse('User View')
+	if 'user' not in request.session:
+		request.session['user'] = ''
+	return render(request, 'UserInfo.html',
+	{
+		'user': request.session['user']
+	})
 
 def login(request):
-	return render(request, "login.html",
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			if username == password:
+				request.session['user'] = username
+				return HttpResponseRedirect(reverse('user:index'))
+			else:
+				return render(request, 'Login.html',
+				{
+					'form': form,
+					'message': 'Username or Password Incorrect!'
+				})
+	return render(request, 'Login.html',
 	{
-		"form": LoginForm()
+		'form': LoginForm()
+	})
+
+def register(request):
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password1 = form.cleaned_data['password1']
+			password2 = form.cleaned_data['password2']
+			address = form.cleaned_data['address']
+			contactInfo = form.cleaned_data['contactInfo']
+			if password1 == password2:
+				request.session['user'] = username
+				return HttpResponseRedirect(reverse('user:login'))
+			else:
+				return render(request, 'Login.html',
+				{
+					'form': form,
+					'message': 'Passwords do not match!'
+				})
+	return render(request, 'Login.html',
+	{
+		'form': RegisterForm()
 	})
