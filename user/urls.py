ccb6b176847e2bb2deef75ed212a1737d49c6685
tauyoung <tauyoung@foxmail.com>
2022-11-28 00:21:15 +0800

    Get rid of Django Forms

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'user'
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('login', views.login, name='login'),
-	path('register', views.register, name='register'),
-	path('<username>', views.userInfo, name='userInfo')
+	path('login', views.login_view, name='login'),
+	path('logout', views.logout_view, name='logout'),
+	path('register', views.register, name='register')
 ]
--- user/views.py
+++ user/views.py
@@ -1,65 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .forms import *
-from .models import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-	if 'user' not in request.session:
-		request.session['user'] = ''
-	return render(request, 'UserInfo.html',
-	{
-		'user': request.session['user']
-		# 'user': User.objects.all()
-	})
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect(reverse('Login'))
+	return render(request, 'UserInfo.html')
 
-def login(request):
+def login_view(request):
 	if request.method == 'POST':
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
-			if username == password:
-				request.session['user'] = username
-				return HttpResponseRedirect(reverse('user:index'))
-			else:
-				return render(request, 'Login.html',
-				{
-					'form': form,
-					'message': 'Username or Password Incorrect!'
-				})
-	return render(request, 'Login.html',
-	{
-		'form': LoginForm()
-	})
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user:
+			login(request, user)
+			return HttpResponseRedirect(reverse('user:index'))
+		else:
+			return render(request, 'Login.html',
+			{
+				'message': 'Invalid Credentials'
+			})
+	return render(request, 'Login.html')
+
+def logout_view(reqeset):
+	logout(reqeset)
+	return HttpResponseRedirect(reverse('user:login'))
 
 def register(request):
 	if request.method == 'POST':
-		form = RegisterForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data['username']
-			password1 = form.cleaned_data['password1']
-			password2 = form.cleaned_data['password2']
-			address = form.cleaned_data['address']
-			contactInfo = form.cleaned_data['contactInfo']
-			if password1 == password2:
-				request.session['user'] = username
-				return HttpResponseRedirect(reverse('user:login'))
-			else:
-				return render(request, 'Login.html',
-				{
-					'form': form,
-					'message': 'Passwords do not match!'
-				})
-	return render(request, 'Login.html',
-	{
-		'form': RegisterForm()
-	})
-
-def userInfo(request, username):
-	user = username
-	return render(request, 'UserInfo.html',
-	{
-		'user': user
-	})
+		username = request.POST['username']
+		password1 = request.POST['password']
+		password2 = request.POST['password2']
+		address = request.POST['address']
+		contactInfo = request.POST['contactInfo']
+		if password1 == password2:
+			request.session['user'] = username
+			return HttpResponseRedirect(reverse('user:login'))
+		else:
+			return render(request, 'Register.html',
+			{
+				'message': 'Passwords do not match!'
+			})
+	return render(request, 'Register.html')
