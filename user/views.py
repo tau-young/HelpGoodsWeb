8f2ed06e44033f65f27044c14f09db5616516c2d
tauyoung <tauyoung@foxmail.com>
2022-11-27 22:00:20 +0800

    Basic user app structure

--- user/views.py
+++ user/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import *
 
 # Create your views here.
 def index(request):
-	return HttpResponse("User View")
+	return HttpResponse('User View')
+
+def login(request):
+	return render(request, "login.html",
+	{
+		"form": LoginForm()
+	})
