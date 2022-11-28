7b8895fc0202133029571a4e7dd66f9fa3ebb3eb
tauyoung <tauyoung@foxmail.com>
2022-11-28 10:58:10 +0800

    Link user models to views

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from . import models
 
 # Create your views here.
 def index(request):
@@ -29,18 +30,25 @@ def logout_view(reqeset):
 	return HttpResponseRedirect(reverse('user:login'))
 
 def register(request):
+	if request.session.get('is_login', None):
+		return HttpResponseRedirect(reverse('user:index'))
 	if request.method == 'POST':
 		username = request.POST['username']
 		password1 = request.POST['password']
 		password2 = request.POST['password2']
 		address = request.POST['address']
 		contactInfo = request.POST['contactInfo']
-		if password1 == password2:
-			request.session['user'] = username
-			return HttpResponseRedirect(reverse('user:login'))
-		else:
-			return render(request, 'Register.html',
-			{
-				'message': 'Passwords do not match!'
-			})
+		if password1 != password2:
+			message = 'Passwords do not match!'
+		if models.User.objects.filter(username=username):
+			message = 'Username taken!'
+		if message:
+			return render(request, 'Register.html', {'message': message})
+		newUser = models.User()
+		newUser.username = username
+		newUser.password = password1
+		newUser.address = address
+		newUser.contactInfo = contactInfo
+		newUser.save()
+		return HttpResponseRedirect(reverse('user:login'))
 	return render(request, 'Register.html')
