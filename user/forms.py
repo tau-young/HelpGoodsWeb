11ac0600ad899770f688ebb7f5a343d144db2a20
tauyoung <tauyoung@foxmail.com>
2022-11-30 22:54:00 +0800

    Remove Lagacy Comments

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
 
 class LoginForm(forms.Form):
 	username = forms.CharField(label='Username')
@@ -10,6 +9,5 @@ class RegisterForm(forms.Form):
 	password = forms.CharField(label='Password', widget=forms.PasswordInput())
 	cpassword = forms.CharField(label='Confirm Password', widget=forms.PasswordInput())
 	address = forms.CharField(label='Address')
-	# phone = PhoneNumberField(region='CN')
 	phone = forms.CharField(label = 'Phone Number')
 	email = forms.EmailField()
