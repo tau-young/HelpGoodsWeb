7ae011d0068b88030bae201a9b5cb92f21a286c9
tauyoung <tauyoung@foxmail.com>
2022-11-30 18:49:03 +0800

    Add Item model

--- itemlist/models.py
+++ itemlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+	itemid = models.AutoField(primary_key=True)
+	categlory = models.CharField(max_length=128, default='Item')
+	itemname = models.CharField(max_length=128)
+	description = models.CharField(max_length=1024)
+	address = models.CharField(max_length=128)
+	phone = models.CharField(max_length=128)
+	email = models.EmailField()
