e68e50dbefbca8fe383e6805c9364584e3d29587
tauyoung <tauyoung@foxmail.com>
2022-12-04 01:46:50 +0800

    Able to select categlory when adding new item

--- item/forms.py
+++ item/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+import inspect
+from . import models
 
 class NewItemForm(forms.Form):
-	categlory = forms.CharField(label='Categlory')
+	categlory = forms.ChoiceField(label='Categlory', choices=[(cate, cate) for cate, _ in inspect.getmembers(models, inspect.isclass) if not cate in ['BaseItem', 'User']])
 	name = forms.CharField(label='Item Name')
 	description = forms.CharField(label='Desciption')
 	address = forms.CharField(label='Address')
