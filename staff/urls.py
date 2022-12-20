4f6d634d992116720dcbdc63477c36cf67f04b63
tauyoung <tauyoung@foxmail.com>
2022-12-20 16:03:15 +0800

    Staff view approve/reject user

--- staff/urls.py
+++ staff/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'staff'
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('approve', views.approve, name='approve'),
+	path('reject', views.reject, name='reject'),
+	path('new', views.new, name='new'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -3,9 +3,36 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+import inspect
+from . import models
+import item.models
 
 # Create your views here.
 @staff_member_required
 def index(request):
-	if request.user.is_staff:
-		return HttpResponse('Staff')
+	inactive_users = []
+	for user in User.objects.filter(is_active=False):
+		inactive_users.append(models.User.objects.get(username=user.username))
+	return render(request, 'Index.html',
+	{
+		'inactive': inactive_users,
+		'categlories': [cate for cate, _ in inspect.getmembers(item.models, inspect.isclass) if not cate in ['Base', 'User']]
+	})
+
+@staff_member_required
+def approve(request):
+	user = User.objects.get(username=request.GET['username'])
+	user.is_active = True
+	user.save()
+	return HttpResponseRedirect(reverse('staff:index'))
+
+@staff_member_required
+def reject(request):
+	User.objects.get(username=request.GET['username']).delete()
+	models.User.objects.get(username=request.GET['username']).delete()
+	return HttpResponseRedirect(reverse('staff:index'))
+
+@staff_member_required
+def new(request):
+	return HttpResponse('New Categlory')
